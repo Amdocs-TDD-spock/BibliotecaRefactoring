28f88fa84c485cd861f14579e60efe36bf412671
dnikitadeshmukh <dnikitadeshmukh@gmail.com>
2012-10-01 00:24:28 +0530

    7th Update

--- src/main/Program.java
+++ src/main/Program.java
@@ -15,10 +15,7 @@ public class Program {
             int userChoice=getUserInput();
 
             if (userChoice == 1) {
-                System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
-                System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
-                System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
-                System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
+                displayBookList();
             } else if (userChoice == 2) {
                 System.out.println(" Please enter the number of the book you wish to checkout: ");
                 int bookChoice = 0;
@@ -89,6 +86,13 @@ public class Program {
                 System.out.println("Enter a valid integer!!");
             }
         }
+    }
+
+    public static void displayBookList() {
+        System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
+        System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
+        System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
+        System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
     }
 
     public static void displayMovieList() {
