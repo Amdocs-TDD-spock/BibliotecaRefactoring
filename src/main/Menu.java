4c5845980c1dbf1b45f69b2f9cdf0e48670924f2
dnikitadeshmukh <dnikitadeshmukh@gmail.com>
2012-10-01 11:51:10 +0530

    10th Update

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -5,7 +5,7 @@ public class Menu {
     private static boolean loggedIn = false;
     private static String savedLibraryNumber = "";
 
-    public static boolean chooseOptionFromMenu() {
+    public boolean chooseOptionFromMenu() {
         int userChoice= Program.getUserInput();
 
         if (userChoice == 1) {
@@ -65,5 +65,21 @@ public class Menu {
     public static void clearLogin() {
         loggedIn = false;
         savedLibraryNumber = "";
+    }
+
+    public static void displayStartMenu() {
+        System.out.println("**********************************************************");
+        System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
+        System.out.println("**********************************************************");
+        System.out.println("*                Menu                                    *");
+        System.out.println("*         =====================                          *");
+        System.out.println("*         1. List Book Catalog                           *");
+        System.out.println("*         2. Check out Book                              *");
+        System.out.println("*         3. Check Library Number                        *");
+        System.out.println("*         4. Movie Listing                               *");
+        System.out.println("*         5. Login                                       *");
+        System.out.println("*         9. Exit                                        *");
+        System.out.println("**********************************************************");
+        System.out.println("Your Selection: ");
     }
 }
